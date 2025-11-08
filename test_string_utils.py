8014c655b637a76c4ed83675e16b6c72402f53ba
Pinok <igorpinok@gmail.com>
2025-11-08 06:32:14 +0100

    Test2.1

---
+++ test_string_utils.py
@@ -0,0 +1,7 @@
+from string_utils import polindrome
+
+def test_polidrom():
+    assert palidrome('stats')
+    assert palidrome('deified')
+    assert not palidrome('hello')
+    assert not palidrome('world')
